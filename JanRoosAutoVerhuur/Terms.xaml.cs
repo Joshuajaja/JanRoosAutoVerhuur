221ce81146a9a2a3c986cdce5c30b35669ca4d11
Celeste <benjamins.stan4@gmail.com>
2026-03-18 14:44:39 +0100

    wooo home page wooo

--- JanRoosAutoVerhuur/Terms.xaml.cs
+++ JanRoosAutoVerhuur/Terms.xaml.cs
@@ -1,70 +1,22 @@
-using JanRoosAutoVerhuur.Models;
-using JanRoosAutoVerhuur.Services;
-using System.Collections.ObjectModel;
-using System.Linq;
-using Microsoft.Maui.Devices;
+using JanRoosAutoVerhuur.Viewmodel;
 
 namespace JanRoosAutoVerhuur;
 
 public partial class Terms : ContentPage
 {
-    public ObservableCollection<Car> Cars { get; set; } = new();
-
-    public int CardSpan { get; set; }
-
-    private readonly CarApiService _carService = new();
+    private readonly MainViewModel _viewModel;
 
     public Terms()
     {
         InitializeComponent();
 
-        // initialize span based on current display
-        UpdateSpan(DeviceDisplay.MainDisplayInfo.Width / DeviceDisplay.MainDisplayInfo.Density);
-
-        // react to display changes (resize / orientation)
-        DeviceDisplay.MainDisplayInfoChanged += (_, __) =>
-        {
-            UpdateSpan(DeviceDisplay.MainDisplayInfo.Width / DeviceDisplay.MainDisplayInfo.Density);
-        };
-
-        BindingContext = this;
-    }
-
-    private void UpdateSpan(double width)
-    {
-        CardSpan = width switch
-        {
-            < 480 => 1,   // phone portrait
-            < 720 => 3,   // phone landscape
-            < 1024 => 4,  // tablets
-            < 1400 => 5,  // small desktop
-            _ => 6        // large desktop
-        };
-
-        OnPropertyChanged(nameof(CardSpan));
+        _viewModel = new MainViewModel();
+        BindingContext = _viewModel;
     }
 
     protected override async void OnAppearing()
     {
         base.OnAppearing();
-
-        var carsFromApi = await _carService.GetCarsAsync();
-
-        Cars.Clear();
-        foreach (var car in carsFromApi)
-            Cars.Add(car);
-    }
-
-    private async void OnCarSelected(object sender, SelectionChangedEventArgs e)
-    {
-        var selectedCar = e.CurrentSelection.FirstOrDefault() as Car;
-
-        if (selectedCar == null)
-            return;
-
-        await Navigation.PushAsync(new CarDetailPage(selectedCar));
-
-        // clear selection so same card can be clicked again
-        ((CollectionView)sender).SelectedItem = null;
+        await _viewModel.LoadCarsAsync();
     }
 }
